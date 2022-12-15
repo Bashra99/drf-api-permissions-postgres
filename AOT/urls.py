8fffed433917194523d44ad85496d6255e60e5c2
Bashra99 <telfahb@gmail.com>
2022-12-15 00:08:09 +0000

    feat: create AOT API using Postgres and django permission

---
+++ AOT/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from .views import AOTListView,AOTDetailView,TitanListView,TitanDetailView
+urlpatterns = [
+    path('',AOTListView.as_view(),name='AOT_list'),
+    path('<int:pk>',AOTDetailView.as_view(),name='AOT_detail'),
+    path('Titan/',TitanListView.as_view(),name='Titan_list'),
+    path('Titan/<int:pk>',TitanDetailView.as_view(),name='Titan_detail')
+]
---
+++ AOT/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+
+from .models import AOT
+from .models import Titan   
+from .serializers import AOTSerializer
+from .serializers import TitanSerializer
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+# Create your views here.
+
+class AOTListView(ListAPIView):
+    queryset = AOT.objects.all()
+    serializer_class= AOTSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
+
+
+
+class AOTDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = AOT.objects.all()
+    serializer_class= AOTSerializer
+    permission_classes=[IsOwnerOrReadOnly]
+
+class TitanListView(ListAPIView):
+    queryset = Titan.objects.all()
+    serializer_class= TitanSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
+
+
+class TitanDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Titan.objects.all()
+    serializer_class= TitanSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
+
+
+
+
+
